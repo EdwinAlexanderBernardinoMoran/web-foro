6d53d103d1938a50da222e8ee5d708bae0b3f5ce
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-03-12 15:58:44 -0600

    Creacion de modelos, migraciones, factories y datos falsos

---
+++ database/migrations/2025_12_16_022210_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+
+            // Un comentario pertenece a un usuario
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            $table->morphs('commentable'); // Los comentarios pueden estar asociados con preguntas o respuestas
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
